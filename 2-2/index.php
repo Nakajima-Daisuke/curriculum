132f5463eefa6fabf49aa61409b420624dda427f
Nakajima-Daisuke <67725375+Nakajima-Daisuke@users.noreply.github.com>
2020-08-26 13:21:43 +0900

    課題提出
    課題2-2

---
+++ 2-2/index.php
@@ -0,0 +1,23 @@
+<?php
+// PHPの研修カリキュラム2-2
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-2
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.API"."<br>";
+echo "※あるコンピュータプログラム（ソフトウェア）の機能や管理するデータなどを、外部の他のプログラムから呼び出して利用するための手順やデータ形式などを定めた規約のこと。"."<br>";
+
+echo "2.SVN (Subversion)"."<br>";
+echo "※著名なオープンソースのバージョン管理システムの一つ。複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードやドキュメントを管理するのに用いられる。CVS（Concurrent Versions System）を参考に開発され、基本的なコマンドなどはCVSと共通になっている。"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
