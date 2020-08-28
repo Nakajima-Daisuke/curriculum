eb5edd9efb5d35e8c6745b186920db70c5921e0d
Nakajima-Daisuke <67725375+Nakajima-Daisuke@users.noreply.github.com>
2020-08-28 17:56:41 +0900

    課題提出
    課題2-4

---
+++ PHP 2章/2-4/index.php
@@ -0,0 +1,25 @@
+<?php
+// PHPの研修カリキュラム2-4
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-4
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.エビデンス"."<br>";
+echo "※証拠、根拠、証言、痕跡などの意味を持つ英単語。ITの分野では、システム開発のテスト工程などで、システムが正しく動作している、あるいは不具合が生じたことを示す証拠となる書類やデータなどをこのように呼ぶことが多い。"."<br>";
+
+echo "2.バッチ"."<br>";
+echo "※複数のプログラムからなる作業において、あらかじめ一連の手順を登録しておき、まとめて連続的に実行する方式。または、一定期間や一定量ごとにデータをまとめて一括して処理する方式。"."<br>";
+
+echo "3.MVCモデル"."<br>";
+echo "※ソフトウェアの設計モデルの一つで、機能を「Model」（モデル）、「View」（ビュー）、「Controller」（コントローラ）の三つの役割に分離して実装し、それらが連携して処理を進める方式。"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
