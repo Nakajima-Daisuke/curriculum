2725ee8bfd050609dd7948df3e1813a5dc8c8bd1
Nakajima-Daisuke <67725375+Nakajima-Daisuke@users.noreply.github.com>
2020-08-31 18:41:56 +0900

    課題提出
    課題2-8

---
+++ PHP 2章/2-8/index.php
@@ -0,0 +1,38 @@
+<?php
+// PHPの研修カリキュラム2-8
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-8
+*/
+?>
+
+<?php
+$fruits = ["appleといったら" =>"りんご", "orangeといったら" => "みかん", "peachといったら" => "もも"];
+foreach ($fruits as $key => $value) {
+    echo $key;
+    echo $value;
+    echo "<br>";
+}
+?>
+
+
+
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.トランザクション"."<br>";
+echo "※取引記録などの意味の他に、ソフトウェアの処理方式の一つで、互いに関連・依存する複数の処理をまとめ、一体不可分の処理単位として扱うことを指す場合が多い。"."<br>";
+
+echo "2.排他ロック"."<br>";
+echo "※データベースシステムなどで記憶領域への同時アクセスを制限するロック機構の一つで、他の実行主体によるアクセスを完全に禁止するもの。"."<br>";
+
+echo "3.チューニング"."<br>";
+echo "※調整、調律、同調などの意味を持つ英単語。日本語の外来語としては、楽器の音の高さの調律・調弦や、無線通信や放送などで、受信したいチャンネルの電波周波数に機器を同調させる操作などを指すことが多い。"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
