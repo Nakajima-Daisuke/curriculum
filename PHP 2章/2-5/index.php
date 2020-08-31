07e7a31f7835c5beaf02a1557629e47fb8388228
Nakajima-Daisuke <67725375+Nakajima-Daisuke@users.noreply.github.com>
2020-08-31 18:27:14 +0900

    課題提出
    課題2-5

--- PHP 2章/2-5/index.php
+++ PHP 2章/2-5/index.php
@@ -26,8 +26,8 @@ if ($name === "taro" && $pass === "pass") {
     echo "パスワードが間違っています。";
 } elseif($pass === "pass") {
     echo "名前が間違っています。";
-} elseif ($name !== "taro" && $pass !== "pass") {
-    echo "入力情報が間違っています。";
+} else {
+    echo "入力内容が間違っています。";
 }
 ?>
 
