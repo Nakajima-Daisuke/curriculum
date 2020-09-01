be25368e3f8306045f5585759813bf534192f81d
Nakajima-Daisuke <67725375+Nakajima-Daisuke@users.noreply.github.com>
2020-09-01 16:37:21 +0900

    課題提出
    課題2-11

---
+++ PHP 2章/2-11/index.php
@@ -0,0 +1,37 @@
+<?php
+// PHPの研修カリキュラム2-11
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-11
+*/
+?>
+
+<form action="result.php" method="post">
+    名前：<input type="text" name="my_name" />
+    <br>
+    メールアドレス：<input type ="text" name="email" />
+    <br>
+    パスワード：<input type="password" name="password" />
+    <br>
+    <input type="submit" value="送信" />
+</form>
+
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.仕様書・設計書"."<br>";
+echo "※仕様書：工業製品やソフトウェア、情報システム、サービスなどを開発する際に、備えるべき機能やその性能、特性、満たすべき要件などを図表や文章などで記述した文書。※設計書：システム開発をおこなう際には設計という工程があります。 この設計の工程というのは、これからシステム開発をしていくに当たって重要になる計画を立てる工程のことです。 その際に作成されるのが設計書（設計ドキュメント）です。 これは「システムをどのように作るか」という点について書かれた資料になります。"."<br>";
+
+echo "2.Git"."<br>";
+echo "※オープンソースの分散バージョン管理システムの一つ。複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードやドキュメントなどの編集履歴を統一的に管理するのに用いられる。"."<br>";
+
+echo "3.マージツール"."<br>";
+echo "※MDLファイル中の既存のマップ式を保持しながら，FDLファイル又はDTDファイルの変更内容をMDLファイルにマージする機能です。 マージ機能を使用することで，再マッピングの作業を最小限に抑えることができます。"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
---
+++ PHP 2章/2-11/result.php
@@ -0,0 +1,26 @@
+<?php
+// PHPの研修カリキュラム2-11
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-11
+*/
+?>
+
+<?php
+$my_name = $_POST['my_name'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは、<?php echo $email; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
+
+
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
