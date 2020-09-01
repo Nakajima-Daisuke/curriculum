ae4f9778a6757bb5f29974a0e09638e7d9839b32
Nakajima-Daisuke <67725375+Nakajima-Daisuke@users.noreply.github.com>
2020-09-01 15:56:40 +0900

    課題提出
    課題2-10

---
+++ PHP 2章/2-10/index.php
@@ -0,0 +1,41 @@
+<?php
+// PHPの研修カリキュラム2-10
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-10
+*/
+?>
+
+
+<!-- 直方体の体積を求める関数を作成してください。
+その関数を使用し、縦=5cm、横=10cm、高さ=8cmの直方体の体積を求めてください。 -->
+<?php
+function getRectangular($vertical,$side,$height) {
+    $area = $vertical * $side * $height;
+    print "直方体の体積は".$area. "です。";
+}
+
+getRectangular(5,10,8);
+?>
+
+
+
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.ハッシュ化"."<br>";
+echo "※元のデータから一定の計算手順に従ってハッシュ値と呼ばれる規則性のない固定長の値を求め、その値によって元のデータを置き換えること。パスワードの保管などでよく用いられる手法である。"."<br>";
+
+echo "2.総合テスト"."<br>";
+echo "※開発中のソフトウェアや情報システムのテスト手法の一つで、開発の最終段階にシステム全体を対象に行われるテスト。"."<br>";
+
+echo "3.デバッグ"."<br>";
+echo "※コンピュータプログラムに潜む欠陥を探し出して取り除くこと。"."<br>";
+
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
